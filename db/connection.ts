acfedbe66a5df870f87a2461d368fd3b010c404f
Imom111 <imom11ivan@gmail.com>
2022-12-21 15:24:57 -0600

    Add db_uri env

--- db/connection.ts
+++ db/connection.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 /* This is creating a new instance of Sequelize. */
 const db = new Sequelize(
-	'mysql://uf7ohogomfidsmbi:gPq0q1o0GMdql0SnLNT8@bsiyueg9lxxg8julclbi-mysql.services.clever-cloud.com:3306/bsiyueg9lxxg8julclbi'
+	String(process.env.DB_URI)
 );
 
 export default db;
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -4,6 +4,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 require('dotenv').config();
 /* This is creating a new instance of Sequelize. */
-const db = new sequelize_1.Sequelize('mysql://uf7ohogomfidsmbi:gPq0q1o0GMdql0SnLNT8@bsiyueg9lxxg8julclbi-mysql.services.clever-cloud.com:3306/bsiyueg9lxxg8julclbi');
+const db = new sequelize_1.Sequelize(String(process.env.DB_URI));
 exports.default = db;
 //# sourceMappingURL=connection.js.map
